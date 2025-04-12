e9a2eb00440d4dc6c496d985af6f6bc9a8be1377
ManuelAlcaz101 <111898585+ManuelAlcaz101@users.noreply.github.com>
2025-04-11 23:36:44 -0700

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -5,19 +5,26 @@ public class Digits
 
 	private ArrayList<Integer> digitList;
 
-	public Digits(int num)
-	{ /* to be implemented in part (a) */ 
-	    digitList = new ArrayList<Integer>();
-	    String line = "" + num;
-	    for(int i = 0; i < line.length(); i++)
-	       digitList.add(line.charAt(i) - '0');
-	}
+public Digits(int num) {
+    digitList = new ArrayList<Integer>();
+    
+    if (num == 0) {
+        digitList.add(0);
+    } else {
+        ArrayList<Integer> temp = new ArrayList<Integer>();
+        while (num > 0) {
+            temp.add(0, num % 10);  // Insert at front to preserve order
+            num /= 10;
+        }
+        digitList.addAll(temp);
+    }
+}
 
 	public boolean isStrictlyIncreasing()
 	{ /* to be implemented in part (b) */
-	    for(int i = 0; i < digitList.size()-1; i++){
-		if(digitList.get(i+1) <= digitList.get(i))
-			return false;
+	 for(int i = 0; i < digitList.size()-1; i++){
+		 if(digitList.get(i+1) <= digitList.get(i))
+			 return false;
 	    }
 	    return true;
 	}
